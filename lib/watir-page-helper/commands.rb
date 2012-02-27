abfb6ac55f0f07f925eeed007432dd89d009ccad
Mark Ryall <mark@ryall.name>
2012-02-27 12:42:47 +1000

    added ripl shell plugin and commands module for dynamic page class loading

---
+++ lib/watir-page-helper/commands.rb
@@ -0,0 +1,50 @@
+require 'forwardable'
+
+module WatirPageHelper
+  module Pages
+    class Page
+      attr_reader :browser
+      extend Forwardable
+
+      def_delegators :@browser, :title, :url, :html, :status, :refresh, :back
+
+      def initialize browser
+        @browser = browser
+      end
+    end
+  end
+
+  class << self
+    attr_accessor :browser
+  end
+
+  module Commands
+    def classify s
+      s.to_s.split('_').map(&:capitalize).join
+    end
+
+    def page_class *args
+      page = WatirPageHelper::Pages::Page.new WatirPageHelper.browser
+      require "watir-page-helper/pages/#{args.join '/'}"
+      mod = WatirPageHelper::Pages
+      args.each do |name|
+        mod = mod.const_get classify name
+      end
+      page.extend mod
+      page
+    end
+
+    def on *args
+      page_class(*args).tap do |p|
+        yield p if block_given?
+      end
+    end
+
+    def visit *args
+      on(*args) do |p|
+        p.goto
+        yield p if block_given?
+      end
+    end
+  end
+end
