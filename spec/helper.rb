a78b346a646684cc194bb62078838ee63e4c36a1
Mark Ryall <mark@ryall.name>
2012-02-27 14:49:05 +1000

    moved page url for tests to a mixin

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,13 +1,3 @@
-require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
 require 'rspec'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
@@ -15,8 +5,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'watir-page-helper'
 require 'watir-webdriver'
 
-module TestHtml
-  def self.path
-    "file://#{File.expand_path(File.dirname(__FILE__))}/test.html"
-  end
+module TestPage
+  include WatirPageHelper
+
+  direct_url "file://#{File.expand_path(File.dirname(__FILE__))}/test.html"
 end
--- spec/watir-page-helper/pages/page_nested_table.rb
+++ spec/watir-page-helper/pages/page_nested_table.rb
@@ -1,7 +1,7 @@
 module WatirPageHelper::Pages::PageNestedTable
   extend WatirPageHelper::ClassMethods
+  include TestPage
 
-  direct_url TestHtml.path
   table :test_table, :id => "mySecondTable"
   row(:test_table_row_1) { |page| page.test_table.tr }
   cell(:test_table_row_1_cell_1) { |page| page.test_table_row_1_row.td }
